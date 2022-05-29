e547f2c736ba8fc29737bb0a944d8d9397d86e17
Joshua Ramczyk <jgram925@gmail.com>
2022-05-29 13:22:30 -0500

    teams working
    teams are working properly

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // Using & Config
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const { raw } = require('express');
 const app = express();
 app.listen(1337);
 app.set('view engine', 'ejs');
-app.use(cors())
+
+const apiKey = "0ca80ddc-63f6-476e-b548-e5fb0934fc4b";
 
 //routing
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home', { apiKey : apiKey });
 })
-app.get('/about', (req, res) => {
-    res.render('about');
+
+app.get('/:teamName/:team', (req, res) => {
+    res.render('team', { apiKey : apiKey, teamInfo : req });
+})
+
+app.get('/:player', (req, res) => {
+    res.render('player', { apiKey : apiKey, playerInfo : req });
 })
