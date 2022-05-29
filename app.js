54bd91c8a330881779d7a2f8dde8475850a898e2
Joshua Ramczyk <jgram925@gmail.com>
2022-05-29 16:12:38 -0500

    cleaned up

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// Using & Config
+// Using
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Config
 app.listen(1337);
 app.set('view engine', 'ejs');
 
-const apiKey = "0ca80ddc-63f6-476e-b548-e5fb0934fc4b";
+// API Key
+const apiKey = '0ca80ddc-63f6-476e-b548-e5fb0934fc4b';
 
 //routing
 app.get('/', (req, res) => {
